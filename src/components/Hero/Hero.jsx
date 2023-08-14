c78fbfbfa0944ed56935b13f54805c832d4cd0f6
kameha1011 <omarcitofc2001@gmail.com>
2023-08-14 10:33:48 -0300

    changed hero letters color

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,14 +4,14 @@ export const Hero = () => {
   return (
     <StyledHero className="container-fluid d-flex flex-column align-items-center justify-content-center position-relative">
       <div className="heroContent position-relative text-center pt-4">
-        <h1 className="display-1">Cool text for engaging the user</h1>
-        <p className="lead fs-4 fw-bold">
+        <h1>Cool text for engaging the user</h1>
+        <p className="lead fw-bold">
           <i>
             &quot;Find your perfect trip designed by insiders who know and love
             their cities &quot;
           </i>
         </p>
-        <StyledButton>View Cities</StyledButton>
+        <StyledButton className="cta-button">View Cities</StyledButton>
       </div>
     </StyledHero>
   );
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -16,8 +16,31 @@ export const StyledHero = styled.header`
     width: 100%;
     height: 100%;
     background-position: center;
+    opacity: 0.85;
+  }
+  h1{
+    font-size: 3rem;
+    letter-spacing: 0.5rem;
+    color: #05014a;
+  }
+  p{
+    color: #03396c;
+    font-size: 1.5rem;
+  }
+  .cta-button{
+    border: 1px solid #05014a;
+    background-color: #05014a;
+    padding: 1rem 1.5rem;
+    font-size: 1.4rem;
+    color: #f6f4eb;
   }
   @media (min-width: 1024px) {
         height: 90vh;
+        h1{
+          font-size: 4rem;
+        }
+        p{
+          font-size: 2rem;
+        }
     }
 `;
