55b867e95b09e6758ee7b4bdc4fe61bae131ae28
kameha1011 <omarcitofc2001@gmail.com>
2023-08-14 10:46:29 -0300

    added social media icons on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import { StyledFooter } from "./StyledFooter";
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 export const Footer = () => {
   return (
     <StyledFooter
@@ -10,6 +12,24 @@ export const Footer = () => {
         <p>Copyright &copy; 2023</p>
       </div>
       <p className="ms-md-auto">Developed by Omar Flores.</p>
+      <div className="d-flex gap-3">
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-decoration-none text-light fs-1"
+        >
+          <BsFacebook />
+        </a>
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-decoration-none text-light fs-1"
+        >
+          <BsInstagram />
+        </a>
+      </div>
     </StyledFooter>
   );
 };
--- src/components/Footer/StyledFooter.js
+++ src/components/Footer/StyledFooter.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const StyledFooter = styled.footer`
     background-color: #91C8E4;
     color: #fff;
-    height: 7rem;
+    min-height: 7rem;
+    padding: 1rem;
     @media(min-width: 768px) {
         height: 5rem;
     }
