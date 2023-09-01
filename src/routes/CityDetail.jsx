a5facece7300bb7a235c3dfe29cf22292f3902b4
kameha1011 <omarcitofc2001@gmail.com>
2023-09-01 12:54:44 -0300

    added button for returning to cities on citydetails

--- src/routes/CityDetail.jsx
+++ src/routes/CityDetail.jsx
@@ -1,8 +1,9 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Itineraries } from "../components/Itineraries/Itineraries";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { get_city_by_id } from "../store/actions/cityActions";
+import { StyledButton } from "../globalStyles";
 export const CityDetail = () => {
   const city = useSelector((store) => store.cityReducer.city);
   const dispatch = useDispatch();
@@ -13,6 +14,13 @@ export const CityDetail = () => {
   return (
     <>
       <header className="d-flex flex-column align-items-center justify-content-center p-2">
+        <aside className="w-100 ">
+          <Link to="/cities" className="text-decoration-none fw-bold">
+            <StyledButton className="fs-4">
+              Go back to cities
+            </StyledButton>
+          </Link>
+        </aside>
         <h1>
           {city.name}, {city.country}
         </h1>
