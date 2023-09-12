ffc172345b1278a26d6d58809368f01290bf5d9a
kameha1011 <omarcitofc2001@gmail.com>
2023-09-12 13:01:46 -0300

    auth services

---
+++ src/api/authService.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const authRequest = axios.create({
+  baseURL: "http://localhost:3000/api/auth",
+});
+
+export const signin = async (data) => {
+  try {
+    const response = await authRequest.post("/signin", data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = async (data) => {
+  try {
+    const response = await authRequest.post("/signup", data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signout = async (token) => {
+  try {
+    await authRequest.post("/signout", {}, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const verify = async (token) => {
+  try {
+    const response = await authRequest.post("/verify", {}, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
