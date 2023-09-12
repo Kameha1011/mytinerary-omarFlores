3d342065369c542f79971ee86e46761d7ade33ff
kameha1011 <omarcitofc2001@gmail.com>
2023-09-12 13:11:50 -0300

    signin button now changes to sign out if user signed in

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,12 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import { FaUserAlt } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 export const Header = () => {
+  const user = useSelector((store) => store.authReducer.user);
+  const location = useLocation();
+  console.log(location);
   return (
     <StyledNavbar expand="md">
       <Container>
@@ -19,13 +23,15 @@ export const Header = () => {
             <Link to="/cities" className="text-decoration-none nav-link">
               Cities
             </Link>
-            <StyledButton
-              variant="primary"
-              className="text-center ms-2 px-3 fw-bold"
-            >
-              <FaUserAlt className="me-2" />
-              Login
-            </StyledButton>
+            <Link to={user ? location.pathname : '/signin'}>
+              <StyledButton
+                variant="primary"
+                className="text-center ms-2 px-3 fw-bold"
+              >
+                <FaUserAlt className="me-2" />
+                {user ? 'Sign Out' : 'Sign In'}
+              </StyledButton>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
